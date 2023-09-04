4134b1a57d887cf6907016c5ed7385af1fc4e5dd
muzijerrymuller <jerrymuller883@gmail.com>
2023-09-04 04:18:35 -0700

    0 rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+
+"""Defines the rectangle class."""
+
+class Rectangle:
+    """Representation of  a rectangle."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+This is more python 
+Jerry Muller
+jerrymuller883@gmail.com
