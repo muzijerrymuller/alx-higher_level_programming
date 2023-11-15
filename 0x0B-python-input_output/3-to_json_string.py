18dbb2f55fbf6bf850864d7bcca6968ee720e92c
muzijerrymuller <jerrymuller883@gmail.com>
2023-11-15 03:59:41 -0800

    jason borne vibes

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,11 +2,7 @@
 """Defines a string-to-JSON function."""
 import json
 
+
 def to_json_string(my_obj):
     """Return the JSON representation of a string object."""
-    try:
-        json_string = json.dumps(my_obj)
-        return json_string
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+    return json.dumps(my_obj)
