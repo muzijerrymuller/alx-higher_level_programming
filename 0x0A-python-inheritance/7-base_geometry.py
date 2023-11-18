b1640d25aa6192a32e2589f5ff6f133c1aeaef8f
muzijerrymuller <jerrymuller883@gmail.com>
2023-11-18 04:01:38 -0800

    wealth of inheritence

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,11 +6,11 @@ class BaseGeometry:
     """BaseGeometry class."""
     def area(self):
         """Method to compute this area."""
-        raise Exception('area() not implemented')
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         """Method for validating the value."""
         if not isinstance(value, int):
-            raise TypeError("{} must be integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
