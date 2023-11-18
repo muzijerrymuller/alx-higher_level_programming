3074ff4f5362f41a64738e24677854b21efda849
muzijerrymuller <jerrymuller883@gmail.com>
2023-11-18 03:49:22 -0800

    aikido things

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""BaseGeometry class."""
 
 
 class BaseGeometry:
-    """Represent base geometry."""
-
+    """BaseGeometry class."""
     def area(self):
-        """Not implemented."""
-        raise Exception("area() is not implemented")
+        """Method to compute said area."""
+        raise Exception('area() not implemented')
