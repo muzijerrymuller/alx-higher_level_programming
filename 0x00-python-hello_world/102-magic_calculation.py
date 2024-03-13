dc98644e0980817fb72baad92affb299728013c0
muzijerrymuller <jerrymuller883@gmail.com>
2024-03-13 05:27:58 -0700

    magical calculations update

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,9 @@
 #!/bin/bash/python3
 python3 -m compileall $PYFILE -b
 
-#task 13: 102-magic_calculation.py
-#!/usr/bin/python3
 def magic_calculation(a, b):
-    return(98 + (a ** b)) 
+    result = 98
+    result = result ** a
+    result = result + b
+    return result
+    return(98 + (a ** b))
