6fbb40ac132d6b42a70d223f1f4e659ee2b924f3
muzijerrymuller <jerrymuller883@gmail.com>
2024-04-18 00:30:05 -0700

    read file python update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+
 def read_file(filename=""):
+    """
+    Read and print the content of a text file to stdout.
+
+    Args:
+        filename (str): The name of the file to be read. Default is an empty string.
+
+    Returns:
+        None
+    """
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
                 print(line, end='')
-                
-    except FileNotFoundError:
-        print("File not found.")
-
+    except FileNotFoundError as e:
+        print(e)
