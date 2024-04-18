8c79e5cbc2239f6cf5d3860b50005f4a4605621d
muzijerrymuller <jerrymuller883@gmail.com>
2024-04-17 23:19:31 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
-"""Defines a text file-reading function."""
-
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
     try:
-        with open(filename, encoding="utf-8") as file:
-            content = file.read()
-            print(content, end="")
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+                
     except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        print("File not found.")
