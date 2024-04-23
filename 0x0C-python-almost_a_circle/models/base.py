dcca5cba84a6a5eaa0d62550b73eab25d6715220
muzijerrymuller <jerrymuller883@gmail.com>
2024-04-23 02:42:55 -0700

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,20 +8,3 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
-if __name__ == "__main__":
-
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
