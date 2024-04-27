7247be6b5fa8748e49667ba6287efbbd8845975b
muzijerrymuller <jerrymuller883@gmail.com>
2024-04-27 07:16:39 -0700

    Updating the class Rectangle by adding validation of all setter methods and instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,6 +24,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if not isinstance (value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError ("value must be > 0")
          self.__width = value
 
     @property
@@ -32,6 +36,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if not isinstance (value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError ("value must be > 0")
          self.__height = value
 
     @property
