2d60450897aeb8ffda6fbc1a3a30b0e04a38af4c
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-02 06:03:02 -0700

    rectangle inherting from base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,11 +12,6 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y 
-
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width <= 0:
