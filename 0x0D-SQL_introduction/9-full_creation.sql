7cf7becb7571931f6712f2f945ae6af41af0576b
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-05 23:39:07 -0700

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,6 @@
 -- creates second_file with props
 
-CREATE TABLE IF NOT EXISTS second_table
-(
-id INT,
-name VARCHAR(256),
-score INT,
-)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- insertion of relevant data
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
