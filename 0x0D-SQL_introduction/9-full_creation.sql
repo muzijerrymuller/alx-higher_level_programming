258a23c4b37f801d30c80df63f3586deced8a2d6
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-05 23:36:44 -0700

    creates second able in the data base

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,10 +2,11 @@
 
 CREATE TABLE IF NOT EXISTS second_table
 (
-	id INT,
-	name VARCHAR(256),
-	score INT,
+id INT,
+name VARCHAR(256),
+score INT,
 )
+
 -- insertion of relevant data
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
