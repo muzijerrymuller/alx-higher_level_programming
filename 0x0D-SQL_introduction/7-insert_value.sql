4899e2a68f24faa0f60e65f73c4ff04ebc150043
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-05 23:15:14 -0700

    inserts a new row in the table first_table (database hbtn_0c_0)

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
--- inserts a new row of data
-INSERT INTO first_table ('id', 'name')
-VALUES
-	(89, "Best school");
+-- inserts a new of data in 'first_table' from 'hbtn_0c_0'
+
+INSERT INTO first_table ('id', 'name') VALUES (89, "Best school");
