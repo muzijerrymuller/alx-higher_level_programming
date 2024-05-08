316062cc73a3625a631d681a864be8452bed4607
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-08 05:58:42 -0700

    Filtered names of States

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database.
+"""
+Module that lists all states from the hbtn_0e_0_usa database
 """
 
 import sys
@@ -8,8 +9,6 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-
-    
 
     c.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
