f39d0402425a56920f18da9cdb4ec199499e0ba9
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-08 05:14:10 -0700

    Stae select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa sorted in ascending order by
-states.id
+Lists all states in ascending order by states.id
 """
-import MySQLdb
+
+
 import sys
+import MySQLdb
 
-
-if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-
+def list_states(mysql_username, mysql_password, db_name):
     try:
         conn = MySQLdb.connect(
             host="localhost",
@@ -21,16 +17,29 @@ if __name__ == "__main__":
             db=db_name,
             charset="utf8"
         )
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row[1])
+
+        cur.close()
+        conn.close()
+
     except MySQLdb.Error as e:
         print("Error connecting to database: {}".format(e))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script_name.py mysql_username mysql_password db_name")
         sys.exit(1)
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    for row in rows:
-        print(row)
-
-    cur.close()
-    conn.close()
+    list_states(mysql_username, mysql_password, db_name)
