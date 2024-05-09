4ce534f671fdacad3677d3e95f7f9ed654ac727e
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-09 05:42:10 -0700

    model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Establish link to table in database.
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
