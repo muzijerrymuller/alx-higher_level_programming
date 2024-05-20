636755f19b9d5effa2aa16345fea19f01b0dfff1
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-20 07:07:28 -0700

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import urllib.request
+import ssl
 
 url = 'https://alx-intranet.hbtn.io/status'
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
 # Use a with statement to ensure the response is properly closed after use
 with urllib.request.urlopen(url) as response:
     body = response.read().decode('utf-8')
