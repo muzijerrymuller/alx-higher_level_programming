ed06a5d6f811dd8e6140ed56b6270ea59a47c190
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-23 09:14:40 -0700

    Constructor method

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,18 +1,10 @@
 #!/usr/bin/env node
+
 'use strict';
-/**
- * A class representing a rectangle.
- */
-class Rectangle {
-  /**
-   * Creates a new Rectangle instance.
-   * @param {number} w - The width of the rectangle.
-   * @param {number} h - The height of the rectangle.
-   */
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
-  }
+
+function Rectangle(w, h) {
+  this.width = w;
+  this.height = h;
 }
 
-export default Rectangle;
+module.exports = Rectangle;
