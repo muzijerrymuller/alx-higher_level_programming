ad03b93003515885809452cf15eae65385832fe4
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-23 08:58:43 -0700

    1 Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+'use strict';
+/**
+ * A class representing a rectangle.
+ */
+class Rectangle {
+  /**
+   * Creates a new Rectangle instance.
+   * @param {number} w - The width of the rectangle.
+   * @param {number} h - The height of the rectangle.
+   */
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+export default Rectangle;
