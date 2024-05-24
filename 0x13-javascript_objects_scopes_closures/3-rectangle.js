77319a4f8eec4ad313437521433a31ebc6c73cb5
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-24 03:49:13 -0700

    Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor(w, h) {
-		if (w > 0 && h > 0) {
-			[this.width, this.height] = [w, h];
-		}
-	}
-}
-print () {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    if (this.width && this.height) {
+      const row = 'X'.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
       }
-      console.log(s);
     }
   }
 }
