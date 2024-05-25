ef4ff53535a7e21f0af27cf9854b71b1f74beeca
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-25 07:25:18 -0700

    Starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,33 +1,23 @@
 #!/usr/bin/node
-const http = require('http');
-const https = require('https');
-const url = require('url');
+
+const got = require('got');
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 if (!movieId) {
   console.error('Usage: ./starwars_movie.js <movie_id>');
   process.exit(1);
 }
 
-const parsedUrl = url.parse(apiUrl);
-const protocol = parsedUrl.protocol === 'https:' ? https : http;
-
-protocol.get(apiUrl, (response) => {
-  let data = '';
-
-  response.on('data', (chunk) => {
-    data += chunk;
+got(url)
+  .then(response => {
+    const movie = JSON.parse(response.body);
+    console.log(movie.title);
+  })
+  catch(error => {
+	if (error.response) {
+		console.log(`Error: ${error.response.statusCode}`);
+	} else {
+		console.error('Error:', error.message);
+	}
   });
-
-  response.on('end', () => {
-    if (response.statusCode === 200) {
-      const movie = JSON.parse(data);
-      console.log(movie.title);
-    } else {
-      console.log(`Error: ${response.statusCode}`);
-    }
-  });
-}).on('error', (error) => {
-  console.error('Error:', error.message);
-});
