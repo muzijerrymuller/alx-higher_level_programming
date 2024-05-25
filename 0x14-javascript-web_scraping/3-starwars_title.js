c9d28c7c6ced6593356e50110f7a5f44ea39fb38
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-25 07:29:09 -0700

    Starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,23 +1,8 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-const got = require('got');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
 
-if (!movieId) {
-  console.error('Usage: ./starwars_movie.js <movie_id>');
-  process.exit(1);
-}
-
-got(url)
-  .then(response => {
-    const movie = JSON.parse(response.body);
-    console.log(movie.title);
-  })
-  catch(error => {
-	if (error.response) {
-		console.log(`Error: ${error.response.statusCode}`);
-	} else {
-		console.error('Error:', error.message);
-	}
-  });
