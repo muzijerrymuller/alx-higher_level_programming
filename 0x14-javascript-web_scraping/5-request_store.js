f0881663a885c8f4264fbd262b131956e7c1c824
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-25 07:46:51 -0700

    Request store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,25 +1,34 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const { get } = require('http');
+const http = require('http');
+const https = require('https');
+const url = require('url');
 
-const url = process.argv[2];
+const inputUrl = process.argv[2];
 const filePath = process.argv[3];
 
-if (!url || !filePath) {
+if (!inputUrl || !filePath) {
   console.error('Usage: ./script.js <URL> <file_path>');
   process.exit(1);
 }
 
-get(url, (response) => {
-  const fileStream = fs.createWriteStream(filePath);
-  response.pipe(fileStream);
+const parsedUrl = url.parse(inputUrl);
+const protocol = parsedUrl.protocol === 'https:' ? https : http;
 
-  fileStream.on('finish', () => {
-    fileStream.close(() => {
-      console.log('File written successfully');
+const request = protocol.get(inputUrl, (response) => {
+  if (response.statusCode >= 300 && response.statusCode < 400 && response.headers.location) {
+    const redirectUrl = response.headers.location;
+    console.log(`Redirecting to ${redirectUrl}`);
+    const redirectProtocol = redirectUrl.startsWith('https') ? https : http;
+    redirectProtocol.get(redirectUrl, (res) => {
+      res.pipe(fs.createWriteStream(filePath));
     });
-  });
+  } else if (response.statusCode === 200) {
+    response.pipe(fs.createWriteStream(filePath));
+  } else {
+    console.error(`Failed to get '${inputUrl}' (${response.statusCode})`);
+  }
 }).on('error', (err) => {
   console.error(`Error: ${err.message}`);
-}); 
+});
