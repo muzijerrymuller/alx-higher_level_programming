ae3888e5014e9658e325d74b91ffd92b801bd682
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-27 04:26:00 -0700

    SQL privlages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
---This Java code effectively lists all privileges for MySQL users user_0d_1 and user_0d_2 on localhost.
+-- This file lists all privileges for MySQL users user_0d_1 and user_0d_2 on localhost.
+
+-- List all privileges for user_0d_1 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List all privileges for user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
