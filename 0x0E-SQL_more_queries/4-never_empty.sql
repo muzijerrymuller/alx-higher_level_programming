8c47fbc7f8a362d1eb409e44942262f2278462e0
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-27 09:47:46 -0700

    Server never empty

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,11 +1,2 @@
 -- Creation of table on mysql server
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
-    name VARCHAR(256)
-);
-
--- Input record values.
-INSERT INTO id_not_null (id, name) VALUES (89, 'Best School');
-
--- Insert record.
-INSERT INTO id_not_null (name) VALUES ('Best');
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
