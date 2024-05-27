62fa988d130a97c04bbf58e4430966b8a9f67c5a
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-27 05:57:32 -0700

    Create user sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,11 +1,8 @@
---Creates MYSQL server user 'user_id_1'
--- Ensure 'user_0d_1' on localhost exists with password 'user_0d_1_pwd'; create if missing.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
--- Provide 'user_0d_1'@'localhost' full access to all databases and update privilege tables.
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- Create MySQL server user 'user_0d_1' if it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases to 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Update the privilege tables
 FLUSH PRIVILEGES;
