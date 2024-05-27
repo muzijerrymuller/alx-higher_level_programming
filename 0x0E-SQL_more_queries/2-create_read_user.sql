9dcad2f7dac6afb039d2f01ffdba10982b2081f1
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-27 06:51:13 -0700

    Generate read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,31 +1,10 @@
 --Generates database hbtn_0d_2
 
--- Check if the database hbtn_0d_2 exists
-SELECT IF(
-    EXISTS (
-        SELECT schema_name FROM information_schema.schemata WHERE schema_name = 'hbtn_0d_2'
-    ),
-    'hbtn_0d_2 exists',
-    'hbtn_0d_2 doesn’t exist'
-) AS db_status;
-
--- Check if the user user_0d_2 exists
-SELECT IF(
-    EXISTS (
-        SELECT user FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'
-    ),
-    'user_0d_2 exists',
-    'user_0d_2 doesn’t exist'
-) AS user_status;
-
--- Generate database hbtn_0d_2 if it doesn't exist
+-- Create database hbtn_0d_2 if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Create SQL user & password
+-- Create user user_0d_2 with password user_0d_2_pwd if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grant privileges
+-- Grant SELECT privilege to user_0d_2 on database hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Output message to try to log in with user_0d_2
-SELECT 'try to login with user_0d_2' AS message;
