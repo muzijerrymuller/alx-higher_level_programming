a030b940a364b78d077091708c4c136d6394540a
muzijerrymuller <jerrymuller883@gmail.com>
2024-05-28 04:21:33 -0700

    no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Script to display all shows in hbtn_0d_tvshows that do not have a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
