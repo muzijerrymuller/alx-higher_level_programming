ca2b8c9c9aeaa49e8568376a30acc1a9c2b81d80
muzijerrymuller <jerrymuller883@gmail.com>
2024-06-09 07:26:21 -0700

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This Python script takes a URL and an email as inputs,sends a POST request to the URL with the email as a parameter,
+This Python script takes a URL and an email as inputs,sends a
+POST request to the URL with the email as a parameter,
 and displays the response body decoded in UTF-8.
 Requirements: use only 'urllib' and 'sys' packages, and the 'with' statement.
 No need to check input arguments. Test with a server on port 5000
