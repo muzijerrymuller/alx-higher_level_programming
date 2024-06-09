931fae43a9d40c52227f596a5bb129e0a9c638c6
muzijerrymuller <jerrymuller883@gmail.com>
2024-06-09 08:11:07 -0700

    holberton status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-This Python script fetches the URL 'https://alx-intranet.hbtn.io/status' using the 'requests' package.
+This Python script fetches the URL
+'https://alx-intranet.hbtn.io/status' using the 'requests' package.
 It displays the body of the response with tabulation before each line.
-Only the 'requests' package is allowed for import; no other packages are permitted.
-The output format resembles the following example (with tabulation before each line):
+Only the 'requests' package is allowed for import;
+no other packages are permitted.
+The output format resembles the following example
+(with tabulation before each line):
     - Body response:
     -     <content of the response>
 """
