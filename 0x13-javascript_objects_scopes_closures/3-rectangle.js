6e71f185d8ae827a4b0c8a36f0da49fe812d8f62
muzijerrymuller <jerrymuller883@gmail.com>
2024-06-10 07:29:47 -0700

    recta

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
-class Rectangle {
+/**
+ * defines a rectangle
+ */
+
+module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-	  }
-	}
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-	print() {
-		if (this.width && this.height) {
-			const row = 'X'.repeat(this.width);
-			for (let i = 0; i < this.height; i++) {
-				console.log(row);
-			}
-		}
-	}
-}
-
-module.exports = Rectangle;
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+};
