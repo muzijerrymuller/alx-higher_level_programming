153e4c3716b3405c864f7243cb86943f501341d5
muzijerrymuller <jerrymuller883@gmail.com>
2024-07-31 05:40:25 -0700

    read and write to js file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,11 @@
 // this code writes a string to a file
 
 const fs = require('fs');
-const file = process.argc[2];
+const file = process.argv[2];
 const write_ = process.argv[3];
 
-fs.writefile(file, write_, 'utf-8', function (error)){
-	if (error){
-		console.log(error);
-	}
+fs.writeFile(file, write_, 'utf-8', function (error) {
+  if (error) {
+    console.log(error);
+  }
 });
