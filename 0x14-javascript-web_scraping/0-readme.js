397d5eacb143603ab294830ec2aa240cfe98de2a
muzijerrymuller <jerrymuller883@gmail.com>
2024-07-31 05:28:30 -0700

    read and write to js file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+//
+//the code reads and prints
+//
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
+// 
+// this code writes a string to a file
+//
 const fs = require('fs');
+const file = process.argc[2];
+const write_ = process.argv[3];
 
-function writeFile (filePath, content) {
-  fs.writeFile(filePath, content, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-}
-
-if (process.argv.length === 4) {
-  const filePath = process.argv[2];
-  const content = process.argv[3];
-  writeFile(filePath, content);
-}
+fs.writefile(file, write_, 'utf-8', function (error)){
+	if (error){
+		console.log(error);
+	}
+});
