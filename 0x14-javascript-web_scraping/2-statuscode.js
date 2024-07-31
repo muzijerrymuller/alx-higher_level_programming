d3146ff7d22d91af1964054f76d0efc414ff68c7
muzijerrymuller <jerrymuller883@gmail.com>
2024-07-31 05:44:36 -0700

    status code return

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
+// Takes a url and returns its status code
 
-const axios = require('axios');
-const targetUrl = process.argv[2];
+const request = require('request');
+const URL = process.argv[2];
 
-if (!targetUrl) {
-  console.error('Usage: ./status_code.js <URL>');
-  process.exit(1);
-}
-
-axios.get(targetUrl, { maxRedirects: 10 })
-  .then(response => {
-    console.log(`code: ${response.status}`);
-  })
-  .catch(error => {
-    if (error.response) {
-      console.log(`code: ${error.response.status}`);
-    } else {
-      console.error('Error:', error.message);
-    }
-  });
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
